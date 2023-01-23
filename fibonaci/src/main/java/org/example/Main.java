4c19fc1cffb6a1ca1fe53276154d2331dd909b62
qvu3 <75177814+qvu3@users.noreply.github.com>
2023-01-23 16:34:39 -0500

    Add Fibonacci Java class

---
+++ fibonaci/src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // get user's input
+        System.out.println("Enter the number of terms: ");
+        int n = scanner.nextInt();
+        if (n <= 0){
+            System.out.println("Please enter a positive number.");
+        }
+        else {
+            int term = fibo(n);
+            System.out.println("The " + n + "th term of the Fibonacci sequence is " + term);
+        }
+    }
+
+    // program to calculate fibonacci sequence using recursion
+    private static int fibo(int n){
+        if (n < 2){
+            return n;
+        }
+        else {
+            return fibo(n-1) + fibo(n-2);
+        }
+    }
+}
